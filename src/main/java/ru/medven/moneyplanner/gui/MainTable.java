01ef9436574f5a1faa5ad5dabcc9a8284555cb32
MeDVen <darkvenomx@gmail.com>
2017-04-11 16:31:19 +0300

    Test table commit.

--- src/main/java/ru/medven/moneyplanner/gui/MainTable.java
+++ src/main/java/ru/medven/moneyplanner/gui/MainTable.java
@@ -1,8 +1,51 @@
 package ru.medven.moneyplanner.gui;
 
+import javax.swing.JTable;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
 /**
  * Main table of money planner program with all operations.
  * Created by medven on 18.12.16.
  */
 public class MainTable {
+
+    private JTable table;
+
+    public MainTable() {
+        TableModel tableModel = initTableModel();
+        table = new JTable(tableModel);
+        RowFilter<Object, Object> filter = new RowFilter<Object, Object>() {
+            public boolean include(Entry entry) {
+                Integer population = (Integer) entry.getValue(1);
+                return population.intValue() > 3;
+            }
+        };
+        TableRowSorter<TableModel> sorter = new TableRowSorter<>(tableModel);
+        sorter.setRowFilter(filter);
+        table.setRowSorter(sorter);
+    }
+
+    public JTable getTable() {
+        return table;
+    }
+
+    private TableModel initTableModel() {
+        Object[][] data = {
+                {"A", 1},
+                {"B", 2},
+                {"C", 3},
+                {"D", 4},
+                {"E", 5}
+        };
+        String[] columnNames = {"Item", "Value"};
+        TableModel tableModel = new DefaultTableModel(data, columnNames) {
+            public Class<?> getColumnClass(int column) {
+                return getValueAt(0, column).getClass();
+            }
+        };
+        return tableModel;
+    }
 }
--- src/main/java/ru/medven/moneyplanner/gui/MainWindow.java
+++ src/main/java/ru/medven/moneyplanner/gui/MainWindow.java
@@ -1,8 +1,13 @@
 package ru.medven.moneyplanner.gui;
 
-import ru.medven.moneyplanner.i18n.Messages;
+import java.awt.BorderLayout;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import ru.medven.moneyplanner.i18n.Messages;
 
 /**
  * Main {@link JFrame} window of money planner.
@@ -13,10 +18,20 @@ public class MainWindow {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame mainFrame = new JFrame(Messages.moneyPlanner);
+
+            MainTable mainTable = new MainTable();
+            JScrollPane jScrollPane = new JScrollPane(mainTable.getTable());
+//            mainFrame.add(jScrollPane);
+
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add(jScrollPane, BorderLayout.CENTER);
+            mainFrame.add(panel);
+
             mainFrame.setSize(1024, 768);
             mainFrame.setVisible(true);
-            mainFrame.setResizable(false);
+            mainFrame.setResizable(true);
             mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mainFrame.pack();
         });
     }
 }
